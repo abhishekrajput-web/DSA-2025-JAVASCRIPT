00325c13a2b3cd6cad5b3d3f01a3509f84bd5f13
Abhishek Rajput <abhishekrajputcr7@gmail.com>
2025-07-11 00:10:22 +0530

    buy sell stocks

---
+++ 1-array/8-best-time-to-buy/1-index.js
@@ -0,0 +1,26 @@
+
+
+
+function bestTimeToBuySellStocks(prices){
+   let n = prices.length;
+   if(n<=0){
+    return 0;
+   }
+
+   let buyPrice = prices[0];
+   let profit = 0;
+
+   for(let i = 1; i < n; i++){
+    if(prices[i] < buyPrice){
+        buyPrice = prices[i];
+    }
+    else{
+        let currentProfit = prices[i] - buyPrice;
+        profit = Math.max(profit, currentProfit);
+    }
+   }
+
+   return profit;
+}
+const prices = [7,1,5,3,6,4];
+console.log(bestTimeToBuySellStocks(prices));
