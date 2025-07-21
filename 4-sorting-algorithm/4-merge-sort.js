86c67d72a46c306fb088afc9ed33540054464dd0
Abhishek Rajput <abhishekrajputcr7@gmail.com>
2025-07-21 22:32:26 +0530

    merge

---
+++ 4-sorting-algorithm/4-merge-sort.js
@@ -0,0 +1,43 @@
+function merge(arr, start, mid, end) {
+  let i = start;
+  let j = mid + 1;
+  let temp = [];
+
+  while (i <= mid && j <= end) {
+    if (arr[i] <= arr[j]) {
+      temp.push(arr[i]);
+      i++;
+    } else {
+      temp.push(arr[j]);
+      j++;
+    }
+  }
+
+  while (i <= mid) {
+    temp.push(arr[i]);
+    i++;
+  }
+
+  while (j <= end) {
+    temp.push(arr[j]);
+    j++;
+  }
+
+  for (let i = 0; i < temp.length; i++) {
+    arr[i + start] = temp[i];
+  }
+}
+
+function mergeSort(arr, start, end) {
+  if (start < end) {
+    let mid = Math.floor(start + (end - start) / 2);
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
+    merge(arr, start, mid, end);
+  }
+
+  return arr;
+}
+
+const arr = [38, 27, 43, 10];
+console.log(mergeSort(arr, 0, arr.length - 1));
